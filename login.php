624980af4c899dd0e70088dfeae16250e0adc5a1
Vichit Pov <vichitpov.vichit@gmail.com>
2017-03-23 23:45:53 +0700

    update code login page

---
+++ login.php
@@ -0,0 +1,25 @@
+<<<<<<< Updated upstream
+=======
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+	<style type="text/css">
+		form{ text-align: center;}
+		input{padding: 10px; margin-bottom: 10px; }
+	</style>
+
+
+</head>
+<body>
+	<form action="information_page.php" method="POST">
+		<label>Username: </label>
+		<input type="text" name="txtUsername" placeholder="Username"><br>
+		<label>Passowrd: </label>
+		<input type="text" name="txtPassword" placeholder="Password"><br>
+		<input type="submit" name="btnSubmit" >
+	</form>
+
+</body>
+</html>
+>>>>>>> Stashed changes
