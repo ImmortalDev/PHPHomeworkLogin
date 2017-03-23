4acb0dd4395c29226b3eef8249df1513da226a0c
Vichit Pov <vichitpov.vichit@gmail.com>
2017-03-23 23:38:03 +0700

    This is my login.php page

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+	$username = $_POST["txtUsername"];
+	$password = $_POST["txtPassword"];
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+ 	<?php
+
+	if ($username == "admin" && $password=="123") {
+ 	 ?>
+ 	 	<h1>Admin</h1>
+ 	 	<h1>Passoword: <?php echo "$password"; ?></h1>
+
+ 	<?php }elseif ($username == "user" && $password == "1234") {
+
+ 	?>
+
+ 		<h1>User</h1>
+ 		<h1>Passoword: <?php echo "$password"; ?></h1>
+
+ 	<?php } ?>
+ </body>
+ </html>
