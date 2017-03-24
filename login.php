b9dffc4fa8d32c289c96f3f0e28cdba96996edf9
Prohos <prohossomrith@gmail.com>
2017-03-24 22:26:42 +0700

    link stylesheet

--- login.php
+++ login.php
@@ -3,6 +3,9 @@
 <html>
 <head>
 	<title>Login Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.js">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 	<style type="text/css">
 		form{ text-align: center;}
 		input{padding: 10px; margin-bottom: 10px; }
@@ -12,8 +15,9 @@
 </head>
 <body>
 	<form action="information_page.php" method="POST">
-		<label>Username: </label>
-		<input type="text" name="txtUsername" placeholder="Username"><br>
+        <label>Username: </label>
+        <input class="form-control" type="text" name="txtUsername" placeholder="Username">
+		<br>
 		<label>Password: </label>
 		<input type="text" name="txtPassword" placeholder="Password"><br>
 		<input type="submit" name="btnSubmit" >
