35bfdd17b80ca37f954d660c5ea2dafb662f57cd
Prohos <prohossomrith@gmail.com>
2017-03-24 23:09:04 +0700

    style coding

--- login.php
+++ login.php
@@ -5,11 +5,8 @@
 	<title>Login Page</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.js">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
-	<style type="text/css">
-		form{ text-align: center;}
-		input{padding: 10px; margin-bottom: 10px; }
-	</style>
 
 
 </head>
@@ -19,8 +16,8 @@
         <input class="form-control" type="text" name="txtUsername" placeholder="Username">
 		<br>
 		<label>Password: </label>
-		<input type="text" name="txtPassword" placeholder="Password"><br>
-		<input type="submit" name="btnSubmit" >
+		<input class="form-control" type="password" name="txtPassword" placeholder="Password"><br>
+		<input class="btn btn-primary mybtn" type="submit" name="btnSubmit" >
 	</form>
 
 </body>
