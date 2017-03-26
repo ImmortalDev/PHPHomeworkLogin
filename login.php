140cafcf6889ab2254be55b4ba11515776fe2437
Vichit Pov <vichitpov.vichit@gmail.com>
2017-03-26 14:13:25 +0700

    a

--- login.php
+++ login.php
@@ -11,7 +11,6 @@
 
 </head>
 <body>
-<<<<<<< Updated upstream
     <div class="login-page">
         <div class="form">
             <form class="register-form" action="information_page.php" method="post">
@@ -29,19 +28,6 @@
             </form>
         </div>
     </div>
-=======
-	<form action="infor
-	mation_page.php" method="POST">
-
-        <label>Username: </label>
-        <input class="form-control" type="text" name="txtUsername" placeholder="Username">
-		<br>
-		<label>Password: </label>
-		<input class="form-control" type="password" name="txtPassword" placeholder="Password"><br>
-		<input class="btn btn-primary mybtn" type="submit" name="btnSubmit" >
-
-	</form>
->>>>>>> Stashed changes
 
 </body>
 </html>
