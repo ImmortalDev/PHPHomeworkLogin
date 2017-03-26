9e96583449b73dec7057a503a730491c2703009a
Vichit Pov <vichitpov.vichit@gmail.com>
2017-03-26 12:10:29 +0700

    login

--- login.php
+++ login.php
@@ -3,21 +3,36 @@
 <html>
 <head>
 	<title>Login Page</title>
+<<<<<<< Updated upstream
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.js">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="style.css">
     <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+=======
+	<style type="text/css">
+		form{ text-align: center;}
+		input{padding: 10px; margin-bottom: 10px; }
+	</style>
+>>>>>>> Stashed changes
 
 
 </head>
 <body>
 	<form action="information_page.php" method="POST">
+<<<<<<< Updated upstream
         <label>Username: </label>
         <input class="form-control" type="text" name="txtUsername" placeholder="Username">
 		<br>
 		<label>Password: </label>
 		<input class="form-control" type="password" name="txtPassword" placeholder="Password"><br>
 		<input class="btn btn-primary mybtn" type="submit" name="btnSubmit" >
+=======
+		<label>Username: </label>
+		<input type="text" name="txtUsername" placeholder="Username"><br>
+		<label>Passowrd: </label>
+		<input type="text" name="txtPassword" placeholder="Password"><br>
+		<input type="submit" name="btnSubmit" >
+>>>>>>> Stashed changes
 	</form>
 
 </body>
