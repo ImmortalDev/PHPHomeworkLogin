7386cb5400fba461ca6b89fad74dafd9c14c2ad4
Prohos <prohossomrith@gmail.com>
2017-03-26 12:09:19 +0700

    style code information_page.php

--- information_page.php
+++ information_page.php
@@ -7,23 +7,30 @@
  <!DOCTYPE html>
  <html>
  <head>
- 	<title></title>
+ 	 <title>Information </title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
  </head>
  <body>
- 	<?php
+    <div class="jumbotron">
+        <div class="container">
+            <?php
 
-	if ($username == "admin" && $password=="123") {
- 	 ?>
- 	 	<h1>Admin</h1>
- 	 	<h1>Passoword: <?php echo "$password"; ?></h1>
+            if ($username == "admin" && $password=="123") {
+                ?>
+                <h1>Admin</h1>
+                <h1>Password: <?php echo "$password"; ?></h1>
 
- 	<?php }elseif ($username == "user" && $password == "1234") {
+            <?php }elseif ($username == "user" && $password == "1234") {
 
- 	?>
+                ?>
 
- 		<h1>User</h1>
- 		<h1>Passoword: <?php echo "$password"; ?></h1>
+                <h1>User</h1>
+                <h1>Password: <?php echo "$password"; ?></h1>
 
- 	<?php } ?>
+            <?php } ?>
+        </div>
+
+    </div>
+
  </body>
  </html>
